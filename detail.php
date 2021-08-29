998c8e7a321ba0efc1cd204293800389fa20dbd0
sonledinh <sonledinh@admicro.vn>
2021-08-30 01:46:37 +0700

    update code

---
+++ detail.php
@@ -0,0 +1,106 @@
+<?php include 'header.php';?>
+<style>
+	header {display: none;}
+</style>
+<main>
+	<section class="main-detail">
+		<div class="header-user">
+	         <div class="container-fluid">
+	            <div class="row align-items-center">
+	            	<div class="col-md-4">
+	            		<div class="logo"><a href=""><img src="images/logo-head.png" class="img-fluid" alt=""></a></div>
+	            	</div>
+	            	<div class="col-md-8 text-right">
+	            		<ul class="justify-content-end">
+			               <li><a href="" class="head-register">Đăng ký</a></li>
+			               <li><a href="" class="head-login">Đăng nhập</a></li>
+			            </ul>
+	            	</div>
+	            </div>
+	         </div>
+	      </div>
+		<div class="banner-detail-thumbs">
+			<div class="avr-thumbs">
+				<img src="images/bn-detail.jpg" class="img-fluid w-100" alt="">
+			</div>
+			<div class="caption-detail">
+				<div class="container">
+					<div class="caption-info">
+						<h1>Khách sạn Eleon 2 - Tập 50</h1>
+						<div class="desc">
+							Totel Eleon 2
+						</div>
+						<div class="thumbs-film"><img src="images/thumbs-film.jpg" class="img-fluid w-100" alt=""></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="content-detail">
+			<div class="container">
+				<div class="top-practice">
+					<div class="avr-prac">
+						<img src="images/film-2.jpg" class="img-fluid w-100" alt="">
+					</div>
+					<div class="info-prac">
+						<div class="list-prac">
+							<div class="item-prac pt-0">
+								<div class="left">Chọn tập</div>
+								<div class="right">
+									<ul class="ssub-prac">
+										<li><a href="">Tập 50</a></li>
+										<li><a href="">Tập 49</a></li>
+										<li><a href="">Tập 48</a></li>
+										<li><a href="">Tập 47</a></li>
+										<li><a href="">Tập 46</a></li>
+										<li><a href="">Tập 45</a></li>
+										<li><a href="">Tập 44</a></li>
+										<li><a href="">Tập 44</a></li>
+										<li><a href="">Tập 43</a></li>
+										<li><a href="">Tập 42</a></li>
+										<li><a href="">Tập 41</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="item-prac d-flex">
+								<div class="left">Phim hài</div>
+								<div class="right">
+									<ul class="list-inline">
+										<li class="list-inline-item"><span>2021</span></li>
+										<li class="list-inline-item"><span>Số tập 50</span></li>
+										<li class="list-inline-item"><span>Độ dài 45 phút</span></li>
+									</ul>
+								</div>
+							</div>
+							<div class="item-prac">
+								<div class="sub-item-prac d-flex">
+									<div class="left">Đạo diễn</div>
+									<div class="right">
+										<p>Anton Fedotov, Anton Maslov, Ekaterina Zabulonskaya</p>
+									</div>
+								</div>
+								<div class="sub-item-prac d-flex">
+									<div class="left">Diễn viên</div>
+									<div class="right">
+										<p>Aleksandra Kuzenkina, Anna Glaube, Diana Pozharskaya, Ekaterina Vilkova, Grigoriy Siyatvinda, Milos Bikovic, Olga Kuzmina, Rina Grishina, Viktor Khorinyak  </p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="detail-info">
+					<div class="box-info-detail">
+						<div class="t-detail">Nội dung phim</div>
+						<p>"Khách sạn Eleon 2" là phần tiếp theo của loạt phim truyền hình nổi tiếng và được yêu thích "Kitchen", được phát trên kênh STS từ 2012 đến 2016. Chúng ta có thể nói rằng đây là phần phụ của bộ phim chuyển thể nhiều phần (phần tiếp theo của một bộ phim nổi tiếng, ảnh hưởng một phần hoặc toàn bộ đến cốt truyện và dàn diễn viên).</p>
+						<p>"Khách sạn Eleon 2" là phần tiếp theo của loạt phim truyền hình nổi tiếng và được yêu thích "Kitchen", được phát trên kênh STS từ 2012 đến 2016. Chúng ta có thể nói rằng đây là phần phụ của bộ phim chuyển thể nhiều phần (phần tiếp theo của một bộ phim nổi tiếng, ảnh hưởng một phần hoặc toàn bộ đến cốt truyện và dàn diễn viên).</p>
+					</div>
+					<div class="box-info-detail">
+						<div class="t-detail">Trailer</div>
+						<p><img src="images/trailer.jpg" alt=""></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>
