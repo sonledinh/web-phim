05d36781b8bff86e40db544641f60d5a58a6c092
sonledinh <sonledinh@admicro.vn>
2021-09-02 23:59:11 +0700

    update detail

--- detail.php
+++ detail.php
@@ -26,10 +26,10 @@
 			<div class="caption-detail">
 				<div class="container">
 					<div class="caption-info">
-						<h1>Khách sạn Eleon 2 - Tập 50</h1>
+						<!-- <h1>Khách sạn Eleon 2 - Tập 50</h1>
 						<div class="desc">
 							Totel Eleon 2
-						</div>
+						</div> -->
 						<div class="thumbs-film"><img src="images/thumbs-film.jpg" class="img-fluid w-100" alt=""></div>
 					</div>
 				</div>
@@ -37,6 +37,59 @@
 		</div>
 		<div class="content-detail">
 			<div class="container">
+				<div class="top-detail">
+					<div class="item-prac d-flex">
+						<div class="left"><span>Server </span></div>
+						<div class="right">
+							<ul class="list-inline">
+								<li class="list-inline-item"><a href="">Server 1</a></li>
+								<li class="list-inline-item"><a href="">Server 2</a></li>
+								<li class="list-inline-item"><a href="">Server 3</a></li>
+								<li class="list-inline-item"><a href="">Server 4</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-prac d-flex">
+						<div class="left"><span>Chọn tập </span></div>
+						<div class="right">
+							<ul class="ssub-prac pt-0">
+								<li><a href="">Tập 50</a></li>
+								<li><a href="">Tập 49</a></li>
+								<li><a href="">Tập 48</a></li>
+								<li><a href="">Tập 47</a></li>
+								<li><a href="">Tập 46</a></li>
+								<li><a href="">Tập 45</a></li>
+								<li><a href="">Tập 44</a></li>
+								<li><a href="">Tập 44</a></li>
+								<li><a href="">Tập 43</a></li>
+								<li><a href="">Tập 42</a></li>
+								<li><a href="">Tập 41</a></li>
+								<li><a href="">Tập 50</a></li>
+								<li><a href="">Tập 49</a></li>
+								<li><a href="">Tập 48</a></li>
+								<li><a href="">Tập 47</a></li>
+								<li><a href="">Tập 46</a></li>
+								<li><a href="">Tập 45</a></li>
+								<li><a href="">Tập 44</a></li>
+								<li><a href="">Tập 44</a></li>
+								<li><a href="">Tập 43</a></li>
+								<li><a href="">Tập 42</a></li>
+								<li><a href="">Tập 41</a></li>
+								<li><a href="">Tập 50</a></li>
+								<li><a href="">Tập 49</a></li>
+								<li><a href="">Tập 48</a></li>
+								<li><a href="">Tập 47</a></li>
+								<li><a href="">Tập 46</a></li>
+								<li><a href="">Tập 45</a></li>
+								<li><a href="">Tập 44</a></li>
+								<li><a href="">Tập 44</a></li>
+								<li><a href="">Tập 43</a></li>
+								<li><a href="">Tập 42</a></li>
+								<li><a href="">Tập 41</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
 				<div class="top-practice">
 					<div class="avr-prac">
 						<img src="images/film-2.jpg" class="img-fluid w-100" alt="">
@@ -44,21 +97,8 @@
 					<div class="info-prac">
 						<div class="list-prac">
 							<div class="item-prac pt-0">
-								<div class="left">Chọn tập</div>
-								<div class="right">
-									<ul class="ssub-prac">
-										<li><a href="">Tập 50</a></li>
-										<li><a href="">Tập 49</a></li>
-										<li><a href="">Tập 48</a></li>
-										<li><a href="">Tập 47</a></li>
-										<li><a href="">Tập 46</a></li>
-										<li><a href="">Tập 45</a></li>
-										<li><a href="">Tập 44</a></li>
-										<li><a href="">Tập 44</a></li>
-										<li><a href="">Tập 43</a></li>
-										<li><a href="">Tập 42</a></li>
-										<li><a href="">Tập 41</a></li>
-									</ul>
+								<div class="caption-info">
+									<h1>Khách sạn Eleon 2 - Tập 50</h1>
 								</div>
 							</div>
 							<div class="item-prac d-flex">
@@ -68,6 +108,23 @@
 										<li class="list-inline-item"><span>2021</span></li>
 										<li class="list-inline-item"><span>Số tập 50</span></li>
 										<li class="list-inline-item"><span>Độ dài 45 phút</span></li>
+									</ul>
+								</div>
+							</div>
+							<div class="item-prac d-flex">
+								<div class="left">Thể loại</div>
+								<div class="right">
+									<p>Drama, Crime, Independent</p>
+								</div>
+							</div>
+							<div class="item-prac d-flex">
+								<div class="left"><span>Tags</span></div>
+								<div class="right">
+									<ul class="list-inline">
+										<li class="list-inline-item"><a href="">Tags 1</a></li>
+										<li class="list-inline-item"><a href="">Tags 2</a></li>
+										<li class="list-inline-item"><a href="">Tags 3</a></li>
+										<li class="list-inline-item"><a href="">Tags 4</a></li>
 									</ul>
 								</div>
 							</div>
@@ -97,6 +154,51 @@
 					<div class="box-info-detail">
 						<div class="t-detail">Trailer</div>
 						<p><img src="images/trailer.jpg" alt=""></p>
+					</div>
+				</div>
+				<div class="list-film-other">
+					<div class="title-other">You May Also Like</div>
+					<div class="row">
+						<div class="col-md-3 col-6 col-sm-3">
+							<div class="item-film film-other">
+								<div class="avarta"><a href=""><img src="images/film-3.jpg" class="img-fluid w-100" alt=""></a></div>
+								<div class="info">
+									<h3><a href="">Khách sạn Eleon 2</a></h3>
+									<p>(2018) · 1 hr 38 min</p>
+									<p>Drama, Crime, Independent</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-6 col-sm-3">
+							<div class="item-film film-other">
+								<div class="avarta"><a href=""><img src="images/film-3.jpg" class="img-fluid w-100" alt=""></a></div>
+								<div class="info">
+									<h3><a href="">Khách sạn Eleon 2</a></h3>
+									<p>(2018) · 1 hr 38 min</p>
+									<p>Drama, Crime, Independent</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-6 col-sm-3">
+							<div class="item-film film-other">
+								<div class="avarta"><a href=""><img src="images/film-3.jpg" class="img-fluid w-100" alt=""></a></div>
+								<div class="info">
+									<h3><a href="">Khách sạn Eleon 2</a></h3>
+									<p>(2018) · 1 hr 38 min</p>
+									<p>Drama, Crime, Independent</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-6 col-sm-3">
+							<div class="item-film film-other">
+								<div class="avarta"><a href=""><img src="images/film-3.jpg" class="img-fluid w-100" alt=""></a></div>
+								<div class="info">
+									<h3><a href="">Khách sạn Eleon 2</a></h3>
+									<p>(2018) · 1 hr 38 min</p>
+									<p>Drama, Crime, Independent</p>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
